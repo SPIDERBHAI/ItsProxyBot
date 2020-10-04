eb55d4f01ec2737173b6d6eb6a8de3c8507af18f
SPIDER_XD <66294171+SPIDERBHAI@users.noreply.github.com>
2020-10-04 21:54:39 +0530

    Update config.py

--- proxybot/config.py
+++ proxybot/config.py
@@ -1,6 +1,6 @@
 class Config:
     LOGGER = True
-    APP_ID = int(os.environ.get("APP_ID", 12345))
+    APP_ID = int(os.environ.get("APP_ID", none))
     API_HASH = os.environ.get("API_HASH", None)
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
     COMMAND_HAND_LER = os.environ.get("COMMAND_HAND_LER", "/")

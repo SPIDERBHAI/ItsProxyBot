7c6af82893f004079f8ff8fe95c7c4fb5b6d0da2
SPIDER_XD <66294171+SPIDERBHAI@users.noreply.github.com>
2020-10-04 21:58:12 +0530

    Update config.py

--- proxybot/config.py
+++ proxybot/config.py
@@ -1,14 +1,14 @@
 class Config:
     LOGGER = True
-    APP_ID = int(os.environ.get("APP_ID", None))
-    API_HASH = os.environ.get("API_HASH", None)
+    APP_ID = int(os.environ.get("APP_ID", "1309301"))
+    API_HASH = os.environ.get("API_HASH", "92ff1ebdc07145c2c1d3d6dc04afccb5")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "1219959468:AAFyqhZoj6IAdZU9bIyJg8aQT24ntxqDU54")
     COMMAND_HAND_LER = os.environ.get("COMMAND_HAND_LER", "/")
     TMP_DIR = os.environ.get("TMP_DIR", "./TEMP/")
     CONTACT_OWNER = os.environ.get("CONTACT_OWNER", None)
     DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "").split())
     BOT_VERSION = os.environ.get("BOT_VERSION", "Development")
-    OWNER_ID = int(os.environ.get("OWNER_ID", None))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "1110765912")
     MESSAGE_DUMP = int(os.environ.get("MESSAGE_DUMP", -100))
 
 
